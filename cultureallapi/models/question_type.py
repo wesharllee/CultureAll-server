fab99340f946a76ca7dde979f775185e02562404
Wesley Harllee <wesharllee@gmail.com>
2022-10-27 15:58:13 -0500

    question_type max_length changed to 30 instead of 20

--- cultureallapi/models/question_type.py
+++ cultureallapi/models/question_type.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class QuestionType(models.Model):
-    type = models.CharField(max_length=20)
+    type = models.CharField(max_length=30)
     companies = models.ManyToManyField("CultUser", through="UserQuestionType", related_name="question_types")
 
 
